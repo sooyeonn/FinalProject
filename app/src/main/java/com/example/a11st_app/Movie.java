858389d677c1183cece63c7414cb078e8e6d5c34
soo07 <soo07131@gmail.com>
2021-12-09 06:17:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a11st_app/Movie.java
@@ -0,0 +1,23 @@
+package com.example.a11st_app;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rnakInten;
+    String rankO1dAndNew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String salesAmt;
+    String salesShare;
+    String salesInten;
+    String salesChange;
+    String salesAcc;
+    String audiCnt;
+    String audiInten;
+    String audiChange;
+    String audiAcc;
+    String scrnCnt;
+    String showCnt;
+
+}
